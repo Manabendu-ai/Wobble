92324ec78d897ee78f4d859b7b062cc9629586d9
Manabendu-ai <rikugithub@gmail.com>
2026-06-13 20:22:40 +0530

    Convert Image to Base64

--- src/main/java/riku/spring/wobble/controller/ImageController.java
+++ src/main/java/riku/spring/wobble/controller/ImageController.java
@@ -1,6 +1,8 @@
 package riku.spring.wobble.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import riku.spring.wobble.service.GeminiService;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/image")
@@ -18,8 +22,10 @@ public class ImageController {
     private final GeminiService geminiService;
 
     @PostMapping("/extract")
-    public ResponseEntity<String> extract(@RequestParam("file")MultipartFile file){
-        return ResponseEntity.ok("Received: "+file.getOriginalFilename());
+    public ResponseEntity<String> extract(@RequestParam("file")MultipartFile file)
+        throws Exception {
+        String base64 = geminiService.extract(file);
+        return ResponseEntity.ok("Received: "+base64);
     }
 
 }
--- src/main/java/riku/spring/wobble/service/GeminiService.java
+++ src/main/java/riku/spring/wobble/service/GeminiService.java
@@ -4,6 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Base64;
 
 @Service
 @RequiredArgsConstructor
@@ -13,4 +16,9 @@ public class GeminiService {
 
     @Value("${gemini.api.key}")
     private String apiKey;
+
+    public String extract(MultipartFile file)throws Exception{
+        byte[] imageBytes = file.getBytes();
+        return Base64.getEncoder().encodeToString(imageBytes).substring(0,100)+"...";
+    }
 }
